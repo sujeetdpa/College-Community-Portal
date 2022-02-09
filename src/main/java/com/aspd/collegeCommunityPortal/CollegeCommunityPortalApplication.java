9ea5b28fe31f0cd00def5267cb856a5e60e85529
Sujeet Mourya <sujeetdpa@gmail.com>
2022-02-09 19:02:20 +0530

    Initial Commits

---
+++ src/main/java/com/aspd/collegeCommunityPortal/CollegeCommunityPortalApplication.java
@@ -0,0 +1,13 @@
+package com.aspd.collegeCommunityPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeCommunityPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeCommunityPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspd/collegeCommunityPortal/CollegeCommunityPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspd.collegeCommunityPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeCommunityPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
