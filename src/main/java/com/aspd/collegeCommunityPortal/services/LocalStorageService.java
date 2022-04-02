88cd53aaef1becc92a02627d5e33081ef5d65d7d
sujeetdpa <sujeetdpa@gmail.com>
2022-04-02 11:46:28 +0530

    [created] classes for LocalStorageService

---
+++ src/main/java/com/aspd/collegeCommunityPortal/services/LocalStorageService.java
@@ -0,0 +1,4 @@
+package com.aspd.collegeCommunityPortal.services;
+
+public interface LocalStorageService {
+}
---
+++ src/main/java/com/aspd/collegeCommunityPortal/services/impl/LocalStorageServiceImpl.java
@@ -0,0 +1,9 @@
+package com.aspd.collegeCommunityPortal.services.impl;
+
+import com.aspd.collegeCommunityPortal.services.LocalStorageService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocalStorageServiceImpl implements LocalStorageService {
+
+}
