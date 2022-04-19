6cf259d476e20fcbf3d6981882bae5f3a58a8d3b
sujeetdpa <sujeetdpa@gmail.com>
2022-04-19 23:08:00 +0530

    [modified] UserUpdateRequest entity

--- src/main/java/com/aspd/collegeCommunityPortal/beans/request/UserUpdateRequest.java
+++ src/main/java/com/aspd/collegeCommunityPortal/beans/request/UserUpdateRequest.java
@@ -9,9 +9,16 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class UserUpdateRequest {
+    private String fullName;
     private String firstName;
     private String lastName;
+    private Integer id;
     private String gender;
+    private String username;
+    private String universityId;
+    private LocalDateTime userCreationTimestamp;
     private LocalDate dob;
+    private LocalDateTime lastLoginTimestamp;
     private String mobileNo;
+    private Integer profileImageId;
 }
