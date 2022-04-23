7f12070e7c34b550d487de3246125b4ef6b9b8c0
sujeetdpa <sujeetdpa@gmail.com>
2022-04-23 20:19:48 +0530

    [fixed] error Cross origin for Admin controller

--- src/main/java/com/aspd/collegeCommunityPortal/controller/AdminController.java
+++ src/main/java/com/aspd/collegeCommunityPortal/controller/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
+@CrossOrigin(origins = "*")
 public class AdminController {
     @Autowired
     private AdminService adminService;
