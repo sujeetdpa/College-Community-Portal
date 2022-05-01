f0be46f41ca8373e7bf64a570c146383c3bad98c
Sujeet Mourya <sujeetdpa@gmail.com>
2022-05-01 23:46:39 +0530

    [modifed] TimeUtil Method

--- src/main/java/com/aspd/collegeCommunityPortal/util/TimeUtil.java
+++ src/main/java/com/aspd/collegeCommunityPortal/util/TimeUtil.java
@@ -12,12 +12,16 @@ public class TimeUtil {
 
     public String getCreationTimestamp(LocalDateTime creationDateTime){
         String result="";
+        long days = ChronoUnit.DAYS.between(creationDateTime, LocalDateTime.now());
         if(creationDateTime.toLocalDate().equals(LocalDate.now())){
             long seconds = Duration.between(creationDateTime.toLocalTime(), LocalTime.now()).getSeconds();
             result=(seconds/3600)+" hours ago";
         }
-        else if(ChronoUnit.DAYS.between(creationDateTime,LocalDateTime.now())<10){
-            result=ChronoUnit.DAYS.between(creationDateTime,LocalDateTime.now())+ " days ago";
+        else if(days<2){
+            result=days+" day ago";
+        }
+        else if(days <10){
+            result=days+ " days ago";
         }
         else{
             result=creationDateTime.toLocalDate().format(DateTimeFormatter.ISO_LOCAL_DATE).toString();
